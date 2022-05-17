879726188351e8c7a944ad1d42c66c2cb095e08a
mic7x4 <mic7x4@gmail.com>
2022-05-17 18:26:29 +0200

    finishing with the header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,12 @@
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
 import './home.css'
 
 function Home() {
   return (
     <div>
-        <div className="container nav__container">
-            <nav>
-                <div className="nav__logo">
-                    <h2>Netphantom</h2>
-                </div>
-                <ul className="nav__menu">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/hotels">Hotels</a></li>
-                    {/* <li><a href="/">Home</a></li> */}
-                </ul>
-            </nav>
-        </div>
+        <Navbar/>
+        <Header/>
     </div>
   )
 }
