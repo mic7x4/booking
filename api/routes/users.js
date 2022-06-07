503903dd32174a146d77700ab37a2d7b421fdb1b
mic7x4 <mic7x4@gmail.com>
2022-06-07 23:59:34 +0200

    checking hotels with admin token

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,21 +5,26 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-    res.send('Hello you are loggged in ')
-})
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//     res.send('Hello you are loggged in ')
+// })
 
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-    res.send('Hello User, You are loggged in and you can delete your account ')
-})
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//     res.send('Hello User, You are loggged in and you can delete your account ')
+// })
 
-router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
-    res.send('Hello Admin, are loggged in and you can delete all accounts ')
-})
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//     res.send('Hello Admin, are loggged in and you can delete all accounts ')
+// })
 
-router.put('/:id',updateUser)
-router.delete('/:id',deleteUser)
-router.get('/:id',getUser)
-router.get('/',getAllUsers)
+router.put('/:id',verifyUser,updateUser)
+
+
+router.delete('/:id',verifyUser,deleteUser)
+
+router.get('/:id',verifyUser,getUser)
+
+
+router.get('/',verifyAdmin, getAllUsers)
 
 export default router
