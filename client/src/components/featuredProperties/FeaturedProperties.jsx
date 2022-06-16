3608b4253eced7d03e876914ff0cd3e66c17af92
mic7x4 <mic7x4@gmail.com>
2022-06-17 01:20:39 +0200

    finishing with the homepage

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,43 +1,30 @@
 import './featuredProperties.css'
 import {image1, image2, image3, image4, image5, image6, image7, image8,image9, image10} from '../../assets/images'
+import useFetch from '../../hooks/useFetch.js';
 
 
 function FeaturedProperties() {
+  const {data,loading,error} = useFetch("hotels?featured=true&limit=4");
+  console.log(data);
   return (
     <div className='faturedProperties'>
-      <div className="fpItem">
-        <img src={image4} alt="" className="featuredProImg" />
-            <span className="fpName">Apart hotel Stare Miasto</span>
-            <span className="fpCity">Millan</span>
-            <span className="fpPrice">Starting from $250</span>
 
+      {loading ? "Loading" : <>
+        {data.map(item => (
+          
+      <div className="fpItem" key={item.id}>
+      <img src={item.photos[0]} alt="" className="featuredProImg" />
+          <span className="fpName">{item.name}</span>
+          <span className="fpCity">{item.city}</span>
+          <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+          {item.rating &&           
             <div className="fpRating">
-                <button>9</button>
-                <span>Excellent</span>
-            </div>
-      </div>
-      <div className="fpItem">
-        <img src={image4} alt="" className="featuredProImg" />
-            <span className="fpName">Apart hotel Stare Miasto</span>
-            <span className="fpCity">Millan</span>
-            <span className="fpPrice">Starting from $250</span>
-
-            <div className="fpRating">
-                <button>9</button>
-                <span>Excellent</span>
-            </div>
-      </div>
-      <div className="fpItem">
-        <img src={image4} alt="" className="featuredProImg" />
-            <span className="fpName">Apart hotel Stare Miasto</span>
-            <span className="fpCity">Millan</span>
-            <span className="fpPrice">Starting from $250</span>
-
-            <div className="fpRating">
-                <button>9</button>
-                <span>Excellent</span>
-            </div>
-      </div>
+              <button>{item.rating}</button>
+              <span>Excellent</span>
+          </div>}
+    </div>
+        ))}
+      </>}
     </div>
   )
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import './navbar.css'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className="navContainer">
-            <span className="logo">Booking</span>
+            <Link to="/" style={{color:"white"}}>
+              <span className="logo">Booking</span>
+            </Link>
             <div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Login</button>
